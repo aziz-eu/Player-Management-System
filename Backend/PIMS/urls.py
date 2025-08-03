070ca1cf06f8e154631bdcbe1f8dfca6c7f698ea
Abdul Aziz <abdulaziz.eu1@gmail.com>
2025-08-04 00:29:29 +0600

    recreate user app

--- Backend/PIMS/urls.py
+++ Backend/PIMS/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
@@ -37,4 +37,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path('account', include("apps.user.urls"))
 ]
